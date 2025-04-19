dccb3131c91db2ee3b1e123861df9c2197f04480
ArpithPaliwal <arpithpaliwal@gmail.com>
2025-04-19 21:04:31 +0530

    second

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import gifImage from '../assets/images/argos_side_photo_0.png';
 import { Link } from 'react-router-dom';
 
